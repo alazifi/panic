c31c9a44254a0fefccf4302d30915df9feb4ca57
alazifi <haidarali.eu@gmail.com>
2025-01-25 11:32:53 +0700

    [change]: add parameter and value

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.use(express.json());
 // Data sementara yang menyimpan title dan body
 let data = {
   id: 2025,
-  name : "default name",
-  addr : "default address",
-  loc : "default location"
+  name : "nama",
+  addr : "alamat",
+  loc : "lokasi",
+  id_device : "Pemangkang-01"
 };
 
 // Endpoint GET
@@ -20,23 +21,25 @@ app.get("/panic", (req, res) => {
     id: data.id,
     name: data.name,
     addr: data.addr,
-    loc: data.loc
+    loc: data.loc,
+    id_device: data.id_device
   });
 });
 
 // Endpoint POST
 app.post("/panic", (req, res) => {
-  const { id, name, addr, loc } = req.body;
+  const { id, name, addr, loc, id_device } = req.body;
 
   // Update nilai parameter yang disimpan
   if (id) data.id = id;
   if (name) data.name = name;
   if (addr) data.addr = addr;
   if (loc) data.loc = loc;
+  if (id_device) data.id_device = id_device;
 
   res.json({
     message: "Post updated successfully!",
-    post: { id: data.id, name: data.name, addr: data.addr, loc: data.loc }
+    post: { id: data.id, name: data.name, addr: data.addr, loc: data.loc, id_device: data.id_device }
   });
 });
 
